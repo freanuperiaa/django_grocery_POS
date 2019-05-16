609bd6bd933351b38e8c119fc3044d204ad727d1
Freanu Peria <magnussonfrancis@gmail.com>
2019-05-16 13:30:48 +0800

    organized project properly. also fixed some unnecessary files.

--- apps/grocery/views.py
+++ apps/grocery/views.py
@@ -1,93 +1,74 @@
-from django.shortcuts import render,get_object_or_404,redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.template.defaultfilters import slugify
 from django.utils import timezone
-
-from .models import Product,Category, Customer
-
-from django.views.generic import (
-    TemplateView,
-    ListView,
-    DetailView,
-)
-from django.views.generic.edit import (
-    CreateView,
-    UpdateView,
-    DeleteView,
-)
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.base import View
-# Create your views here.
+
+from .models import Product, Customer
 
 
 class HomePageView(ListView):
-    template_name = 'home.html'
+    template_name = 'grocery/home.html'
 
     def get_queryset(self):
         query_set = Product.objects.order_by('-no_purchases')[:5]
-
         return query_set
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['time'] = timezone.now()
-        
         return context
 
+
 class ProductsListView(ListView):
-    template_name = "products_list.html"
-    
+    template_name = "grocery/products_list.html"
+
     def get_queryset(self):
         query_set = Product.objects.order_by('name')
         return query_set
 
+
 class ProductsDetailView(View):
     
-    def get(self, request, *args, **kwargs):
-        item = get_object_or_404(Product, slug = kwargs['slug'])
+    def get(self, request, **kwargs):
+        item = get_object_or_404(Product, slug=kwargs['slug'])
         context = {
-            'item' : item,
+            'item': item,
         }
-
-        return render(request, 'product_detail.html', context)
+        return render(request, 'grocery/product_detail.html', context)
 
 
 class ProductsCreateView(CreateView):
-    template_name = 'create_product.html'
+    template_name = 'grocery/create_product.html'
     model = Product
-    fields = [
-        'name',
-        'category',
-        'price',
-        'stocks',
-        'no_purchases',
-        'description',
-    ]
+    fields = ['name', 'category', 'price', 'stocks',
+              'no_purchases', 'description', ]
 
-    def form_valid(self,form):
+    def form_valid(self, form):
         obj = form.save(commit=False)
         obj.pub_date = timezone.now()
         obj.slug = slugify(obj.name)
         obj.save()
         return redirect(obj.get_absolute_url())
 
+
 class ProductUpdateView(UpdateView):
-    template_name = 'update_product.html'
+    template_name = 'grocery/update_product.html'
     model = Product
-    fields = [
-        'category',
-        'price',
-        'stocks',
-        'description',
-    ]
+    fields = ['category', 'price', 'stocks', 'description', ]
+
 
 class CustomersListView(ListView):
-    template_name = 'customers_list.html'
+    template_name = 'grocery/customers_list.html'
     
     def get_queryset(self):
         query_set = Customer.objects.order_by('first_name')
         return query_set
 
+
 class CustomersCreateView(CreateView):
-    template_name = 'create_customer.html'
+    template_name = 'grocery/create_customer.html'
     model = Customer
     fields = [
         'first_name', 'last_name',
@@ -95,17 +76,14 @@ class CustomersCreateView(CreateView):
         'trxn_made', 'mem_type'
     ]
 
-
     def form_valid(self,form):
         obj = form.save(commit=False)
         obj.mem_date = timezone.now()
         obj.save()
         return redirect('customers')
 
+
 class CustomersUpdateView(UpdateView):
-    template_name = 'customer_update.html'
+    template_name = 'grocery/customer_update.html'
     model = Customer
-    fields = [
-        'first_name', 'last_name',
-        'gender', 'address', 'mem_type',
-    ]
+    fields = ['first_name', 'last_name', 'gender', 'address', 'mem_type', ]
--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('', include('grocery.urls')),
+    path('', include('apps.grocery.urls')),
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
+    path('users/', include('apps.users.urls')),
     path('users/', include('django.contrib.auth.urls')),
 ]
